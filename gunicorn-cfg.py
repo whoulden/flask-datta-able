01e44c6a0d2f1f52983e2ab86770c08ef8e0a2be
Wayne Houlden <whoulden@janison.com.au>
2022-09-16 16:42:40 +1000

    Deploy4

--- gunicorn-cfg.py
+++ gunicorn-cfg.py
@@ -6,6 +6,6 @@ Copyright (c) 2019 - present AppSeed.us
 bind = '0.0.0.0:8080'
 workers = 1
 accesslog = '-'
-loglevel = 'debug'
+loglevel = 'info'
 capture_output = True
 enable_stdio_inheritance = True
